2fb2ceb4954896547b25318c0faa0a295c249519
ALEKSian <aleIIIka92@gmail.com>
2012-07-13 11:36:17 +0400

    SIMPLE MODELS

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,8 @@
 class Micropost < ActiveRecord::Base
+<<<<<<< HEAD
+  belongs_to :user
+  validates :content, :length => { :maximum => 140 }
+=======
+>>>>>>> 6a1fa9db24dc19a8e269cf1280a245bb9481823b
   attr_accessible :content, :user_id
 end
