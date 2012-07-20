2f26d7e64b70f8bfda08f84083d05152906cf91f
ALEKSian <aleIIIka92@gmail.com>
2012-07-20 13:10:58 +0400

    registration witherror mesages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
-   def show
+  def show
     @user = User.find(params[:id])
   end
-
   def new
+    @user = User.new
     @title = "Sign up"
+  end
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the best blog app in the world!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
   end
 end
